78d73143189063a5ddc6c362ff1ecab8836aef33
発財皇帝 <88429315+facaikotei@users.noreply.github.com>
2025-04-13 16:32:30 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,9 @@ const config = {
             position: "left",
             label: "Tutorial",
           },
+          /*
           { to: "/blog", label: "Blog", position: "left" },
+          */
           {
             href: `https://github.com/${organizationName}/${projectName}`,
             label: "GitHub",
