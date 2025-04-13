16808fb846db776924d2c01fd51f9766fa480861
発財皇帝 <88429315+facaikotei@users.noreply.github.com>
2025-04-13 17:41:25 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,13 +14,6 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
       </div>
     </header>
   );
@@ -29,10 +22,7 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      //title={`Hello from ${siteConfig.title}`}
-      //description="Description will go into a meta tag in <head />"
-      >
+    <Layout>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
