ab8cfadb4164d633bb49c6237af1fd7531136d0c
発財皇帝 <88429315+facaikotei@users.noreply.github.com>
2025-04-14 13:20:17 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home(): JSX.Element {
         <iframe
           style={{
             width:"-webkit-fill-available",
-            maxWidth:"240",
+            maxWidth:240,
           }}
           src="https://drive.google.com/embeddedfolderview?id=1uc4Y4HKf4RmRBiaxT5O6zkZdkfcAzgtA"
           height={480}
