c64363a1def157ea608acfbef7d5014c85a82d8c
発財皇帝 <88429315+facaikotei@users.noreply.github.com>
2025-04-14 12:27:19 +0800

    Update index.tsx

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -15,8 +15,14 @@ const FeatureList: FeatureItem[] = [
     //Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        <iframe
+          style={{
+            width:"-webkit-fill-available",
+            maxWidth:"640",
+            height:"100%",
+          }}
+          src="https://drive.google.com/embeddedfolderview?id=1uc4Y4HKf4RmRBiaxT5O6zkZdkfcAzgtA"
+        ></iframe>
       </>
     ),
   },
