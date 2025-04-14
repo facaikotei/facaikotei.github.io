2a5d8283f886522dd27c26b9661005c708213744
発財皇帝 <88429315+facaikotei@users.noreply.github.com>
2025-04-14 12:24:23 +0800

    Update index.tsx

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  //Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
 };
 
@@ -45,8 +45,8 @@ const FeatureList: FeatureItem[] = [
 
 function Feature({title, description}: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center padding-horiz--md">
+    <div /*className={clsx('col col--4')}*/>
+      <div /*className="text--center padding-horiz--md"*/>
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
@@ -56,9 +56,9 @@ function Feature({title, description}: FeatureItem) {
 
 export default function HomepageFeatures(): JSX.Element {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
+    <section /*className={styles.features}*/>
+      <div /*className="container"*/>
+        <div /*className="row"*/>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
